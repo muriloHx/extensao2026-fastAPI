424e72d14e41e0cea36480e05d41a3fcae83ca84
MuriloHx <twitterfo131@gmail.com>
2026-02-13 00:24:37 -0300

    adicao de endpoints + alteracao na estrutura das tabelas

--- create_db.py
+++ create_db.py
@@ -4,10 +4,9 @@ with sqlite3.connect("db.sqlite3") as conn:
     conn.executescript("""
     PRAGMA foreign_keys = ON;
 
-    CREATE TABLE IF NOT EXISTS alunos (
+    CREATE TABLE IF NOT EXISTS turmas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        turma TEXT NOT NULL
+        nome TEXT NOT NULL UNIQUE 
     );
 
     CREATE TABLE IF NOT EXISTS jogos (
@@ -17,7 +16,7 @@ with sqlite3.connect("db.sqlite3") as conn:
 
     CREATE TABLE IF NOT EXISTS sessoes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        aluno_id INTEGER NOT NULL,
+        turma_id INTEGER NOT NULL,
         jogo_id INTEGER NOT NULL,
         palavra TEXT,
         dificuldade TEXT,
@@ -25,7 +24,7 @@ with sqlite3.connect("db.sqlite3") as conn:
         acertos INTEGER,
         erros INTEGER,
         data_execucao DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (aluno_id) REFERENCES alunos(id) ON DELETE CASCADE,
+        FOREIGN KEY (turma_id) REFERENCES turmas(id) ON DELETE CASCADE,
         FOREIGN KEY (jogo_id) REFERENCES jogos(id) ON DELETE CASCADE
     );
     """)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session as SessionType
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.sql.roles import JoinTargetRole
 
 # --------------------
 # Configuração do banco
@@ -24,12 +26,19 @@ Base = declarative_base()
 # --------------------
 
 
-class AlunoModel(Base):
-    __tablename__ = "alunos"
+class TurmasModel(Base):
+    __tablename__ = "turmas"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, nullable=False)
-    turma = Column(String, nullable=False)
+    nome = Column(String, nullable=False, unique=True)
+
+
+class JogosModel(Base):
+    __tablename__ = "jogos"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(
+        String,
+    )
 
 
 # Cria as tabelas
@@ -40,9 +49,12 @@ Base.metadata.create_all(bind=engine)
 # --------------------
 
 
-class AlunoCreate(BaseModel):
+class TurmasCreate(BaseModel):
     nome: str
-    turma: str
+
+
+class JogosCreate(BaseModel):
+    nome: str
 
 
 # --------------------
@@ -62,12 +74,48 @@ def get_db():
 app = FastAPI()
 
 
-@app.post("/aluno")
-def add_aluno(aluno: AlunoCreate, db: SessionType = Depends(get_db)):
-    novo = AlunoModel(nome=aluno.nome, turma=aluno.turma)
-
+@app.post("/turma")
+def add_turma(turma: TurmasCreate, db: SessionType = Depends(get_db)):
+    novo = TurmasModel(nome=turma.nome)
     db.add(novo)
-    db.commit()
-    db.refresh(novo)
+    try:
+        db.commit()
+        db.refresh(novo)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Turma '{turma.nome}' já existe")
 
     return novo
+
+
+@app.delete("/turma/{turma_id}")
+def delete_turma(turma_id: int, db: SessionType = Depends(get_db)):
+    turma = db.query(JogosModel).filter(JogosModel.id == turma_id).first()
+    if not turma:
+        raise HTTPException(status_code=400, detail=f"Turma não encontrada")
+
+
+@app.post("/jogo")
+def add_jogo(jogo: JogosCreate, db: SessionType = Depends(get_db)):
+    novo = JogosModel(nome=jogo.nome)
+    db.add(novo)
+    try:
+        db.commit()
+        db.refresh(novo)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Jogo '{jogo.nome}' já existe")
+
+    return novo
+
+
+@app.delete("/jogo/{jogo_id}")
+def delete_jogo(jogo_id: int, db: SessionType = Depends(get_db)):
+    jogo = db.query(JogosModel).filter(JogosModel.id == jogo_id).first()
+    if not jogo:
+        raise HTTPException(status_code=404, detail="Jogo não encontrado")
+
+    # Remove do banco
+    db.delete(jogo)
+    db.commit()
+    return {"detail": f"Jogo {jogo.nome} deletado com sucesso"}
