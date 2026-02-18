f52106f09beb829f078c34cb3cdead8b6597f0af
MuriloHx <twitterfo131@gmail.com>
2026-02-18 00:22:06 -0300

    Dashboard: add filtro periodo + botao download dataframe

--- dashboard/streamlit.py
+++ dashboard/streamlit.py
@@ -1,12 +1,55 @@
 import streamlit as st
 import requests
 import pandas as pd
+from datetime import datetime
 from streamlit_autorefresh import st_autorefresh
+
+st.set_page_config(
+    page_title="Dashboard",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": None,
+        "Report a bug": None,
+        "About": None,
+    }
+)
+
 
 
 API_URL = "http://127.0.0.1:8000/api/internal"
 
 st_autorefresh(interval=10000, key="healthcheck")  # recarrega o script a cada 10 segundos
+
+@st.dialog("Baixar dataframe atual (.CSV)", width="medium")
+def download_dialog():
+    st.subheader("Você está prestes a baixar o dataframe atual")
+    st.write("Filtros ativos:")
+    filtros = pd.DataFrame(
+        {
+            "Anos": [anos_sel if anos_sel else "Nenhum filtro"],
+            "Turmas": [turma_sel if turma_sel else "Nenhum filtro"],
+            "Jogos": [jogos_sel if jogos_sel else "Nenhum filtro"],
+            "Periodo": [
+                f"{data_inicio_sel} -> {data_fim_sel}"
+                if (data_inicio_sel is not None and data_fim_sel is not None)
+                else "Nenhum filtro"
+            ],
+        },
+        index=["Filtros Ativos"]
+    )
+
+    st.table(filtros)
+
+    st.download_button(
+        "Baixar CSV",
+        data=lambda: df.to_csv(index=False).encode("utf-8"),
+        file_name=f"dados{datetime.now().strftime("%Y-%M-%D")}.csv",
+        mime="text/csv",
+        type="primary",
+        width="stretch"
+    )
 
 def check_api_health():
     try:
@@ -72,61 +115,72 @@ df["data_execucao"] = pd.to_datetime(df["data_execucao"])
 
 
 # ---- Sidebar filtros ----
-st.sidebar.header("Filtros")
-#filtro anos e turma
-col1, col2 = st.sidebar.columns(2)
-with col1:
-    anos_sel = st.multiselect(
-        "Ano",
-        sorted(df["ano"].dropna().unique()),
-        placeholder="Selecione anos"
-
-    )
-with col2:
-    turma_sel = st.multiselect(
-        "Turma",
-        sorted(df["turma"].dropna().unique()),
-        placeholder="Selecione turmas"
-    )
-#filtro jogos
-jogos_sel = st.sidebar.multiselect(
-    "Jogo",
-    sorted(df["jogo_nome"].unique()),
-    placeholder="Selecione jogos"
-)
-
-#sel turma
-if anos_sel:
-    df = df[df["ano"].isin(anos_sel)]
-
-if jogos_sel:
-    df = df[df["jogo_nome"].isin(jogos_sel)]
-
-#filtro periodo
-col1, col2 = st.sidebar.columns([3,1], vertical_alignment="bottom")
-data_min = df["data_execucao"].min().date()
-data_max = df["data_execucao"].max().date()
-data_inicio_sel, data_fim_sel = None, None
-try:
+with st.sidebar.expander("Filtros"):
+    #filtro anos e turma
+    col1, col2 = st.columns(2)
     with col1:
-        data_inicio_sel, data_fim_sel = st.date_input(
-            "Período",
-            value=(data_min, data_max),
-            min_value=data_min,
-            max_value=data_max,
-            key = "periodo"
+        anos_sel = st.multiselect(
+            "Ano",
+            sorted(df["ano"].dropna().unique()),
+            placeholder="Selecione anos"
+
         )
-except ValueError:
-    pass
-if data_inicio_sel is not None and data_fim_sel is not None:
-    mascara = ((df["data_execucao"].dt.date >= data_inicio_sel) &
-        (df["data_execucao"].dt.date <= data_fim_sel))
-    df = df[mascara]
-with col2:
-    st.button("↻", width="stretch", help="Recarrega o periodo completo",
-        on_click=lambda: st.session_state.update(
-            {"periodo": (data_min, data_max)}
-        ))
+    with col2:
+        turma_sel = st.multiselect(
+            "Turma",
+            sorted(df["turma"].dropna().unique()),
+            placeholder="Selecione turmas"
+        )
+    #filtro jogos
+    jogos_sel = st.multiselect(
+        "Jogo",
+        sorted(df["jogo_nome"].unique()),
+        placeholder="Selecione jogos"
+    )
+
+    #sel turma
+    if anos_sel:
+        df = df[df["ano"].isin(anos_sel)]
+
+    if jogos_sel:
+        df = df[df["jogo_nome"].isin(jogos_sel)]
+
+    #filtro periodo
+    col1, col2 = st.columns([3,1], vertical_alignment="bottom")
+    data_min = df["data_execucao"].min().date()
+    data_max = df["data_execucao"].max().date()
+    data_inicio_sel, data_fim_sel = None, None
+
+    if "periodo" not in st.session_state:
+        st.session_state["periodo"] = (data_min, data_max)
+
+    try:
+        with col1:
+            data_inicio_sel, data_fim_sel = st.date_input(
+                "Período",
+                value=(data_min, data_max),
+                min_value=data_min,
+                max_value=data_max,
+                key = "periodo"
+            )
+    except ValueError:
+        pass
+    if data_inicio_sel is not None and data_fim_sel is not None:
+        mascara = ((df["data_execucao"].dt.date >= data_inicio_sel) &
+            (df["data_execucao"].dt.date <= data_fim_sel))
+        df = df[mascara]
+    with col2:
+        st.button("↻", width="stretch", help="Recarrega o periodo completo",
+            on_click=lambda: st.session_state.update(
+                {"periodo": (data_min, data_max)}
+            ))
+
+
+
+
+
+st.sidebar.button("Baixar CSV", on_click=download_dialog, type="primary",width="stretch")
+
 
 
 # ---- KPIs ----
