b44c04b021cf00c2c08134a777733e17eaf1d37e
MuriloHx <twitterfo131@gmail.com>
2026-02-22 18:39:24 -0300

    API: checar se banco existe em config.py

--- api/config.py
+++ api/config.py
@@ -9,5 +9,8 @@ API_KEY = os.getenv("API_KEY")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set")
 
+if not os.path.exists(DATABASE_URL):
+    raise RuntimeError("Banco de dados não encontrado")
+
 if not API_KEY:
     raise ValueError("API_KEY is not set")
